395041e1045716ab3eccc33692709e4d5c97597f
chenxing <mitrecx@163.com>
2021-10-06 11:52:31 +0800

    test Profile

---
+++ learn-3-common-config/src/main/java/cn/mitrecx/learn3commonconfig/profile/Main.java
@@ -0,0 +1,18 @@
+package cn.mitrecx.learn3commonconfig.profile;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+
+        context.getEnvironment().setActiveProfiles("dev");
+        context.register(ProfileConfig.class);
+        context.refresh();
+
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        System.out.println(demoBean.getContent());
+        context.close();
+    }
+}
