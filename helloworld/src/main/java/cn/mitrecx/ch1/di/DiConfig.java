81b553cff7598d27437591c045013ca689e7ba4c
chenxing <mitrecx@163.com>
2021-10-06 11:51:09 +0800

    Revert "Test PostConstruct and PreDestroy while constructing a bean"
    This reverts commit 50c3bdc2

--- helloworld/src/main/java/cn/mitrecx/ch1/di/DiConfig.java
+++ helloworld/src/main/java/cn/mitrecx/ch1/di/DiConfig.java
@@ -3,7 +3,7 @@ package cn.mitrecx.ch1.di;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-// @Configuration 声明当前类是一个配置类, 相当于一个 Spring 配置的 xml 文件
+// @Configuration 声明当前类是一个配置类
 @Configuration
 // @ComponentScan 自动扫描包下 所有使用 @Service,
 // @Component, @Repository, @Controller 的类,
